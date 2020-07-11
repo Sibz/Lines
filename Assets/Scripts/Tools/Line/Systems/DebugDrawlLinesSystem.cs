29032d40df4c97d18ddeefdc646c3d32ec72d0e7
Sibz <12050358+Sibz@users.noreply.github.com>
2020-07-11 12:30:08 +1000

    Added Debug draw system

---
+++ Assets/Scripts/Tools/Line/Systems/DebugDrawlLinesSystem.cs
@@ -0,0 +1,33 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using UnityEngine;
+
+namespace Sibz.Lines.Tools.Systems
+{
+    public class DebugDrawlLinesSystem : SystemBase
+    {
+        private EntityQuery sectionsQuery;
+        protected override void OnCreate()
+        {
+            sectionsQuery = GetEntityQuery(typeof(LineSection));
+            RequireForUpdate(sectionsQuery);
+        }
+
+        protected override void OnUpdate()
+        {
+            using (var sections =
+                sectionsQuery.ToComponentDataArrayAsync<LineSection>(Allocator.TempJob, out JobHandle jh))
+            {
+                jh.Complete();
+                for (int i = 0; i < sections.Length; i++)
+                {
+                    var s = sections[i];
+                    Debug.DrawLine(s.Bezier.c0, (Vector3)s.Bezier.c0 + Vector3.up * 1.5f, Color.blue);
+                    Debug.DrawLine(s.Bezier.c1, (Vector3)s.Bezier.c1 + Vector3.up * 1.5f, Color.cyan);
+                    Debug.DrawLine(s.Bezier.c2, (Vector3)s.Bezier.c2 + Vector3.up * 1.5f, Color.blue);
+                }
+            }
+        }
+    }
+}
