0aa29a61ba099df0f81e94275d5b0d888bbfaa75
Sibz <12050358+Sibz@users.noreply.github.com>
2020-07-11 13:00:27 +1000

    Updated knot data to have flags

--- Assets/Scripts/Tools/Line/Components/LineKnotData.cs
+++ Assets/Scripts/Tools/Line/Components/LineKnotData.cs
@@ -6,5 +6,13 @@ namespace Sibz.Lines
     public struct LineKnotData : IBufferElementData
     {
         public float3 Knot;
+        public KnotFlags Flags;
+    }
+
+    public enum KnotFlags : ushort
+    {
+        None = 0,
+        End = 1,
+        Other = 2
     }
 }
