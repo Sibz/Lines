e0ac593a051bfa221fb2a63e8aded83b9f8ba872
Sibz <12050358+Sibz@users.noreply.github.com>
2020-07-12 00:34:56 +1000

    Added LineProfile

---
+++ Assets/Scripts/ECS/Lines/Components/LineProfile.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Entities;
+
+namespace Sibz.Lines.ECS.Components
+{
+    public struct LineProfile : IComponentData
+    {
+        public float MaxCurveAngularChange;
+        public float MaxGradientAngularChange;
+        public float CollisionWidth;
+        public float CollisionHeight;
+        public float CollisionDepth;
+    }
+}
