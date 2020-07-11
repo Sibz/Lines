a97a5961807ae0f089e90a844dfba6d75c00846e
Sibz <12050358+Sibz@users.noreply.github.com>
2020-07-11 20:19:43 +1000

    Added join functions to LineJoinPoint.cs

--- Assets/Scripts/Tools/Line/Components/LineJoinPoint.cs
+++ Assets/Scripts/Tools/Line/Components/LineJoinPoint.cs
@@ -18,5 +18,36 @@ namespace Sibz.Lines
                 JoinData.ConnectedEntity.Equals(other.JoinData.ConnectedEntity) &&
                 JoinData.ConnectedIndex.Equals(other.JoinData.ConnectedIndex);
         }
+
+        public static void Join(
+            BufferFromEntity<LineJoinPoint> buffer,
+            NewJoinInfo joinInfo)
+            => Join(buffer[joinInfo.FromSection], buffer[joinInfo.ToSection], joinInfo);
+
+
+        public static void Join(
+            DynamicBuffer<LineJoinPoint> fromBuffer,
+            DynamicBuffer<LineJoinPoint> toBuffer,
+            NewJoinInfo joinInfo)
+        {
+            Update(fromBuffer, joinInfo.FromIndex, joinInfo.ToSection, joinInfo.ToIndex);
+            Update(toBuffer, joinInfo.ToIndex, joinInfo.FromSection, joinInfo.FromIndex);
+        }
+
+        private static void Update(DynamicBuffer<LineJoinPoint> buff, int fromIndex, Entity toEntity, int toIndex)
+        {
+            var join = buff[fromIndex];
+            join.JoinData.ConnectedEntity = toEntity;
+            join.JoinData.ConnectedIndex = toIndex;
+            buff[fromIndex] = join;
+        }
+
+        public struct NewJoinInfo
+        {
+            public Entity FromSection;
+            public Entity ToSection;
+            public int FromIndex;
+            public int ToIndex;
+        }
     }
 }
