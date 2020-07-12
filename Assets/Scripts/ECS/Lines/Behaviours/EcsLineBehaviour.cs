41b883deeb5ccd41258ebc5ca1851fa5e5f9e59b
Sibz <12050358+Sibz@users.noreply.github.com>
2020-07-12 17:37:09 +1000

    Updated line prefab, added node prefab

--- Assets/Scripts/ECS/Lines/Behaviours/EcsLineBehaviour.cs
+++ Assets/Scripts/ECS/Lines/Behaviours/EcsLineBehaviour.cs
@@ -1,5 +1,7 @@
-﻿using Unity.Entities;
+﻿using System;
+using Unity.Entities;
 using UnityEngine;
+using Object = System.Object;
 
 namespace Sibz.Lines.ECS.Behaviours
 {
@@ -9,5 +11,35 @@ namespace Sibz.Lines.ECS.Behaviours
         public EcsLineNodeBehaviour EndNode2;
 
         public Entity LineEntity;
+
+        private bool destroyCorrectly;
+
+        public void Destroy()
+        {
+            if (LineWorld.Em.Exists(EndNode1.JoinPoint))
+            {
+                LineWorld.Em.DestroyEntity(EndNode1.JoinPoint);
+            }
+            if (LineWorld.Em.Exists(EndNode2.JoinPoint))
+            {
+                LineWorld.Em.DestroyEntity(EndNode2.JoinPoint);
+            }
+            if (LineWorld.Em.Exists(LineEntity))
+            {
+                LineWorld.Em.DestroyEntity(LineEntity);
+            }
+            destroyCorrectly = true;
+            Destroy(this);
+        }
+
+        private void OnDestroy()
+        {
+            if (!destroyCorrectly)
+            {
+                Debug.LogWarning(
+                    "Line was not destroyed using EcsLineBehaviour.Destroy method. "+
+                    "This will not clean up relevant entities");
+            }
+        }
     }
 }
