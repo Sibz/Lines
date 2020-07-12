c64c9064da93980e4745119eb1bca963f73980e3
Sibz <12050358+Sibz@users.noreply.github.com>
2020-07-13 01:22:16 +1000

    Added knot spacing to line profile

--- Assets/Scripts/ECS/Lines/Components/LineProfile.cs
+++ Assets/Scripts/ECS/Lines/Components/LineProfile.cs
@@ -9,5 +9,14 @@ namespace Sibz.Lines.ECS.Components
         public float CollisionWidth;
         public float CollisionHeight;
         public float CollisionDepth;
+        public float KnotSpacing;
+
+        public static LineProfile Default()
+        {
+            return new LineProfile
+            {
+                KnotSpacing = 0.25f
+            };
+        }
     }
 }
