fafb87377607eaa00b98a92eb1e8828d28388163
Sibz <12050358+Sibz@users.noreply.github.com>
2020-07-13 15:01:39 +1000

    Mesh now builds triangles in look-back pattern

--- Assets/Scripts/ECS/Lines/Jobs/LineMeshRebuildJob.cs
+++ Assets/Scripts/ECS/Lines/Jobs/LineMeshRebuildJob.cs
@@ -32,32 +32,34 @@ namespace Sibz.Lines.ECS.Jobs
                 float3 cross = math.normalize(math.cross(up, direction));
                 float zMod = cross.z * halfWidth;
                 float xMod = cross.x * halfWidth;
-                int vertexIndex1 = i * 2;
+
 
                 VertexData.Add(new MeshVertexData
                 {
                     Position = new float3(knot.x - xMod, knot.y, knot.z - zMod),
-                    Normal = new float3(0,1,0),
-                    Uv = new float2(0, (float) i / (Knots.Length - 1))
-                });
-                VertexData.Add(new MeshVertexData
-                {
-                    Position = new float3(knot.x + xMod, knot.y, knot.z + zMod),
-                    Normal = new float3(0,1,0),
+                    Normal = new float3(0, 1, 0),
                     Uv = new float2(0, (float) i / (Knots.Length - 1))
                 });
 
-                if (i == Knots.Length - 1)
+                VertexData.Add(new MeshVertexData
+                {
+                    Position = new float3(knot.x + xMod, knot.y, knot.z + zMod),
+                    Normal = new float3(0, 1, 0),
+                    Uv = new float2(0, (float) i / (Knots.Length - 1))
+                });
+                int vertexIndex1 = VertexData.Length - 1;
+
+                if (i == 0)
                 {
                     continue;
                 }
 
-                Triangles.Add(new MeshTriangleData { VertexIndex = vertexIndex1 + 1 });
-                Triangles.Add(new MeshTriangleData { VertexIndex = vertexIndex1 + 3 });
-                Triangles.Add(new MeshTriangleData { VertexIndex = vertexIndex1 + 2 });
-                Triangles.Add(new MeshTriangleData { VertexIndex = vertexIndex1 + 1 });
-                Triangles.Add(new MeshTriangleData { VertexIndex = vertexIndex1 + 2 });
+                Triangles.Add(new MeshTriangleData { VertexIndex = vertexIndex1 - 2 });
                 Triangles.Add(new MeshTriangleData { VertexIndex = vertexIndex1 });
+                Triangles.Add(new MeshTriangleData { VertexIndex = vertexIndex1 - 1 });
+                Triangles.Add(new MeshTriangleData { VertexIndex = vertexIndex1 - 2 });
+                Triangles.Add(new MeshTriangleData { VertexIndex = vertexIndex1 - 1 });
+                Triangles.Add(new MeshTriangleData { VertexIndex = vertexIndex1 - 3 });
             }
         }
     }
