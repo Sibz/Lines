d1bc19cffa2020fcffb1351f49e9fadbefed263d
Sibz <12050358+Sibz@users.noreply.github.com>
2020-07-18 14:09:27 +1000

    Added NewLineUpdateModifiersJob

---
+++ Assets/Scripts/ECS/Lines/Jobs/Line/NewLineUpdateModifiersJob.cs
@@ -0,0 +1,41 @@
+﻿using Sibz.Lines.ECS.Components;
+using Sibz.Lines.ECS.Events;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+
+namespace Sibz.Lines.ECS.Jobs
+{
+    public struct NewLineUpdateModifiersJob : IJobParallelFor
+    {
+        [ReadOnly]
+        public NativeArray<Entity> LineEntities;
+        [ReadOnly]
+        public NativeArray<NewLineUpdateEvent> UpdateEvents;
+        [ReadOnly]
+        public ComponentDataFromEntity<NewLine> NewLines;
+
+        // In order to use the updating information in other jobs
+        // we must provide an array, or the other jobs must run next frame
+        [WriteOnly, NativeDisableParallelForRestriction]
+        public NativeArray<NewLine> UpdatedNewLines;
+
+        public EntityCommandBuffer.Concurrent Ecb;
+
+        public void Execute(int index)
+        {
+            var newLine = NewLines[LineEntities[index]];
+            var modifiers = UpdateEvents[index].Modifiers;
+
+            newLine.Modifiers.EndHeights.x += modifiers.EndHeights.x;
+            newLine.Modifiers.EndHeights.y += modifiers.EndHeights.y;
+            newLine.Modifiers.InnerHeights.x += modifiers.InnerHeights.x;
+            newLine.Modifiers.InnerHeights.y += modifiers.InnerHeights.y;
+
+            Ecb.SetComponent(index, LineEntities[index], newLine);
+
+            UpdatedNewLines[index] = newLine;
+        }
+    }
+}
