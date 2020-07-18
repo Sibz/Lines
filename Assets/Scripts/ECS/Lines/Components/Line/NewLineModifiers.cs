c521f2077e953b94bf1b261b3ab872e233943155
Sibz <12050358+Sibz@users.noreply.github.com>
2020-07-18 13:47:49 +1000

    Added NewLineModifiers struct

---
+++ Assets/Scripts/ECS/Lines/Components/Line/NewLineModifiers.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Mathematics;
+
+namespace Sibz.Lines.ECS.Components
+{
+    public struct NewLineModifiers
+    {
+        /// <summary>
+        /// End heights for each end point on a new line
+        /// </summary>
+        public float2 EndHeights;
+        /// <summary>
+        /// Inner heights are used to create arcs or bridge like height adjustments
+        /// They are essentially the control points in the height bezier
+        /// </summary>
+        public float2 InnerHeights;
+    }
+}
