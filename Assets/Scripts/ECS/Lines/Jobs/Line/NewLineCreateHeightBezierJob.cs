bb6716e025a643659e36f2c0c2a3dd28951894b2
Sibz <12050358+Sibz@users.noreply.github.com>
2020-07-18 15:01:50 +1000

    Updated NewLineCreateHeightBezierJob Now the y axis only describes difference in height

--- Assets/Scripts/ECS/Lines/Jobs/Line/NewLineCreateHeightBezierJob.cs
+++ Assets/Scripts/ECS/Lines/Jobs/Line/NewLineCreateHeightBezierJob.cs
@@ -1,8 +1,5 @@
-﻿using System.Runtime.CompilerServices;
-using ICSharpCode.NRefactory.Ast;
-using Sibz.Lines.ECS.Components;
+﻿using Sibz.Lines.ECS.Components;
 using Unity.Collections;
-using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
 
@@ -22,19 +19,18 @@ namespace Sibz.Lines.ECS.Jobs
         public void Execute(int index)
         {
             var modifiers = UpdatedNewLines[index].Modifiers;
-            var startPos = LineWithJoinData[index].JoinPointA.Pivot;
-            var endPos = LineWithJoinData[index].JoinPointB.Pivot;
-            var length = math.distance(startPos, endPos);
-
-            var point1 = new float2(0, startPos.y + modifiers.EndHeights.x);
-            var point2 = new float2(length, endPos.y+ modifiers.EndHeights.y);
+            var startPos  = LineWithJoinData[index].JoinPointA.Pivot;
+            var endPos    = LineWithJoinData[index].JoinPointB.Pivot;
+            var length    = math.distance(startPos, endPos);
+            var point1    = new float2(0, modifiers.EndHeights.x);
+            var point2    = new float2(length, modifiers.EndHeights.y);
             var controlPoint1 = new float2(length * modifiers.InnerDistances.x,
                                            math.lerp(point1.y, point2.y, modifiers.InnerDistances.x)
                                          + modifiers.InnerHeights.x);
             var controlPoint2 = new float2(length - length * modifiers.InnerDistances.y,
                                            math.lerp(point1.y, point2.y, 1 - modifiers.InnerDistances.y)
                                          + modifiers.InnerHeights.y);
-             HeightBeziers[index] = new float2x4(point1, controlPoint1, controlPoint2, point2);
+            HeightBeziers[index] = new float2x4(point1, controlPoint1, controlPoint2, point2);
         }
     }
 }
