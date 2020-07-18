e3432670e0f8aa21c960b8b6a5ace3b83243ed5c
Sibz <12050358+Sibz@users.noreply.github.com>
2020-07-18 14:53:17 +1000

    Updated NewLine to have InnerDistances field

--- Assets/Scripts/ECS/Lines/Components/Line/NewLineModifiers.cs
+++ Assets/Scripts/ECS/Lines/Components/Line/NewLineModifiers.cs
@@ -8,10 +8,21 @@ namespace Sibz.Lines.ECS.Components
         /// End heights for each end point on a new line
         /// </summary>
         public float2 EndHeights;
+
         /// <summary>
         /// Inner heights are used to create arcs or bridge like height adjustments
         /// They are essentially the control points in the height bezier
         /// </summary>
         public float2 InnerHeights;
+
+        public float2 InnerDistances;
+
+        public static NewLineModifiers Defaults()
+        {
+            return new NewLineModifiers
+                   {
+                       InnerDistances = new float2(0.33f, 0.33f)
+                   };
+        }
     }
 }
